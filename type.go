828419b4169300800e43abd0401c0ffa91e49008
Maulana Imanulhaq <imanulhaq5@gmail.com>
2023-11-13 01:34:26 +0700

    update

--- type.go
+++ type.go
@@ -71,23 +71,3 @@ type Response struct {
 	Message string      `json:"message" bson:"message"`
 	Data    interface{} `json:"data" bson:"data"`
 }
-
-//GIS
-
-type GeoJsonPoint struct {
-	Type       string        `json:"type" bson:"type"`
-	Properties Properties    `json:"properties" bson:"properties"`
-	Geometry   GeometryPoint `json:"geometry" bson:"geometry"`
-}
-
-type GeoJsonLineString struct {
-	Type       string             `json:"type" bson:"type"`
-	Properties Properties         `json:"properties" bson:"properties"`
-	Geometry   GeometryLineString `json:"geometry" bson:"geometry"`
-}
-
-type GeoJsonPolygon struct {
-	Type       string          `json:"type" bson:"type"`
-	Properties Properties      `json:"properties" bson:"properties"`
-	Geometry   GeometryPolygon `json:"geometry" bson:"geometry"`
-}
